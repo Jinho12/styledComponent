8dd391c954d7a320e2d17a02492c3faa4b4e906a
JinHo Shin <jinho@draph.ai>
2022-12-27 15:54:13 +0900

    4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 //백틱 안에는 CSS폼으로 ; 이고 따옴표 사용 div를 변경하여  CSS 부분을 사용하고 싶다면? as를 사용
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: "10px" })`
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  display: flex;
   background-color: tomato;
+  height: 200px;
+  width: 200px;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 3s linear infinite;
+  span {
+    font-size: 50px;
+    &:hover {
+      font-size: 40px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
     // as를 사용하여 div => as로 변경 가능
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😍</span>
+      </Box>
+    </Wrapper>
   );
 }
 
