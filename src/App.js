3a530c4c91d29873b29963788337646b437b1e88
JinHo Shin <jinho@draph.ai>
2022-12-27 14:35:17 +0900

    3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import styled from "styled-components";
 
-//백틱 안에는 CSS폼으로 ; 이고 따옴표 사용
+//백틱 안에는 CSS폼으로 ; 이고 따옴표 사용 div를 변경하여  CSS 부분을 사용하고 싶다면? as를 사용
 const Father = styled.div`
   display: flex;
 `;
-// 컴포넌트의 props를 가져와 사용할 수 있음
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-// Box에 있는 것을 가져와서 다음과 같이 사용할 수 있음
-const Circle = styled(Box)`
-  border-radius: 50px;
+
+const Input = styled.input.attrs({ required: true, minLength: "10px" })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="grey" />
+    // as를 사용하여 div => as로 변경 가능
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
