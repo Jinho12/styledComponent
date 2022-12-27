f5c64960ca046dcd6bfc476016afabf274aa2d7f
JinHo Shin <jinho@draph.ai>
2022-12-27 14:14:54 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import styled from "styled-components";
 
+//백틱 안에는 CSS폼으로 ; 이고 따옴표 사용
 const Father = styled.div`
   display: flex;
 `;
-
-const BoxOne = styled.div`
-  display: flex;
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
+// 컴포넌트의 props를 가져와 사용할 수 있음
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
-
-const Text = styled.span`
-  color: white;
+// Box에 있는 것을 가져와서 다음과 같이 사용할 수 있음
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="grey" />
     </Father>
   );
 }
